5918cb3da6dc1e42a002b10483c24b57c71f54d9
jiyu13 <jiyucoding@yahoo.com>
2025-10-13 17:11:28 -0400

    feat: register authentication routes

--- urls.py
+++ urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', AppUserView.as_view(), name='users'),
-    path('register/', AppUserRegisterView.as_view(), name='register')
+    path('register/', AppUserRegisterView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('auth/', include('rest_framework.urls')),
 ]
+
