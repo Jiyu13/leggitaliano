4442bb2003a3753c88330ace1e099b1238f88c3e
jiyu13 <jiyucoding@yahoo.com>
2025-11-04 22:59:36 -0500

    - add remove button - handleDeleteTranslationItem function to remove unwanted string from translation item -check if item is empty before moving to sentence

--- frontend/src/components/DictionaryTranslationItems.jsx
+++ frontend/src/components/DictionaryTranslationItems.jsx
@@ -22,46 +22,60 @@ function DictionaryTranslationItems({
         setTransItems(updatedItems)
     }
 
-    function handleMoveToSentences(index) {
-        setTextareaError(null)
-
-        const targetItem = transItems.filter((ti, i) => index === i)
-        const splitItem = targetItem[0].split(",")
-        const targetSentence = splitItem[0]
-        const sentenceTranslation = splitItem[1]
-        const updatedItems = translationItems.filter((original_item, i) => index !== i)
-
+    function handleDeleteTranslationItem(tran_item, index) {
+        const updatedTransItems = translationItems.filter((original_item, i) => index !== i)
         const data = {
-            word: clickedWord,
-            word_type: wordType,
-            translation: updatedItems,
+            word_id: wordId,
             translation_index: translationIndex,
-            sentence: targetSentence.trim(),
-            sentence_translation: sentenceTranslation.trim()
+            translation: updatedTransItems,
         }
-        console.log(data)
-        api.post(`/sentence/add/${wordId}/`, data)
+        api.patch(`/word/update_translation/${wordId}/`, data)
            .then(res => {
-                const result = res.data
-               // console.log("result--------------", result.data)
-
-                const newSentence = result["sentence"]
-                const updatedWord = result["word"]
-                const updatedWords = dictionaryWords.map((dw) => dw.id === updatedWord.id ? updatedWord : dw)
+               const result = res.data
+               const updatedWords = dictionaryWords.map((dw) => dw.id === result.id ? result : dw)
                setDictionaryWords(updatedWords)
             })
             .catch(error => {
-               if (error.response) {
-                // server responded with error status
+               if (error.response) {// server responded with error status
                console.log(error.response.data.error);
-              } else {
-                // network / CORS / other failure
+              } else {// network / CORS / other failure
                 console.log("network error", error.message);
             }})
+    }
 
+    function handleMoveToSentences(index) {
+        setTextareaError(null)
 
         if (transItems[index] === "") {
             setTextareaError([index,"Cannot be empty."])
+        } else {
+            const targetItem = transItems.filter((ti, i) => index === i)
+            const splitItem = targetItem[0].split(",")
+            const targetSentence = splitItem[0]
+            const sentenceTranslation = splitItem[1]
+            const updatedItems = translationItems.filter((original_item, i) => index !== i)
+
+            const data = {
+                word: clickedWord,
+                word_type: wordType,
+                translation: updatedItems,
+                translation_index: translationIndex,
+                sentence: targetSentence.trim(),
+                sentence_translation: sentenceTranslation.trim()
+            }
+            api.post(`/sentence/add/${wordId}/`, data)
+               .then(res => {
+                    const result = res.data
+                    const updatedWord = result["word"]
+                    const updatedWords = dictionaryWords.map((dw) => dw.id === updatedWord.id ? updatedWord : dw)
+                   setDictionaryWords(updatedWords)
+                })
+                .catch(error => {
+                   if (error.response) {
+                   console.log(error.response.data.error);
+                  } else {
+                    console.log("network error", error.message);
+                }})
         }
     }
     return (
@@ -80,13 +94,26 @@ function DictionaryTranslationItems({
                         <RequiredWarning>{textareaError[1]}</RequiredWarning>
                     )}
 
-                    <FilledButton
-                        style={{border: "1px solid #fff", marginTop: "8px"}}
-                        onClick={ () => handleMoveToSentences(index)}
-                    >
-                        Move to sentences
-                    </FilledButton>
-                        {/*<button>done</button>*/}
+                    <div style={{display: "flex", gap:"0.5rem"}}>
+                        <FilledButton
+                            // disabled={transItems[index] === ""}
+                            style={{
+                                border: "1px solid #fff", marginTop: "8px",
+                                //cursor: transItems[index] === "" ? "no-drop" : "pointer",
+                            }}
+                            onClick={ () => handleMoveToSentences(index)}
+                        >
+                            Move to sentences
+                        </FilledButton>
+
+                        <FilledButton
+                            style={{border: "1px solid #fff", marginTop: "8px"}}
+                            onClick={ () => handleDeleteTranslationItem(tran_item, index)}
+                        >
+                            Delete
+                        </FilledButton>
+                    </div>
+
 
 
                 </TranslationItem>
