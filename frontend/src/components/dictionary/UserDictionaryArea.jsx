2fff64047fdc6375c0fb2c57d423531bd645fdd9
jiyu13 <jiyucoding@yahoo.com>
2026-01-16 20:31:59 -0500

    - add search bar jsx + functionality

--- frontend/src/components/dictionary/UserDictionaryArea.jsx
+++ frontend/src/components/dictionary/UserDictionaryArea.jsx
@@ -1,56 +1,140 @@
 import styled from "styled-components";
 import search_icon from "../../assets/icons/search_icon.svg";
-import DictionaryWordItem from "./DictionaryWordItem";
-import DictionaryWordNewMeaningForm from "./DictionaryWordNewMeaningForm";
 import {useState} from "react";
 import UserDictionaryWordItem from "./UserDictionaryWordItem";
 import {HeadSectionContainer, Ipa, NotFoundContainer, Word} from "./StaffDictionaryArea";
+import api from "../../api";
+import UserDictionarySearchResult from "./UserDictionarySearchResult";
 
 
 function UserDictionaryArea({
-    ipa, setIpa, clickedWord,clickedWordIndex, dictionaryWords, setDictionaryWords, wordNotFound, setNotFound,
+    ipa, clickedWord, dictionaryWords, wordNotFound, setNotFound,
+    searchInputData, setSearchInputData, searchResult, setSearchResult
 }) {
 
     const [showMeaningId, setShowMeaningId] = useState(null)
-    const [showEditFormId, setShowEditFormId] = useState(null)
+    const [searchError, setSearchError] = useState(null)
+
+
+    function handleInputChange(e) {
+        const value = e.target.value
+        if (value === "") {
+            setSearchInputData("")
+            setSearchResult(null)
+        } else {
+            setSearchInputData(value)
+        }
+    }
+
+    function handleSearch(e) {
+        e.preventDefault()
+
+        setSearchError(null)
+
+        api.get(`/word/word/${searchInputData}/`)
+            .then(res => {
+                const result = res.data
+                result.word = searchInputData
+                setSearchResult(result)
+            })
+            .catch (error => {
+                if (error.response) {
+                    setSearchError(error.response.data.error);
+                } else {
+                    console.log("network error", error.message);
+                }})
+    }
+
+    const wordToShow = searchResult !== null ? searchResult.word : clickedWord
+    const ipaToShow = searchResult !== null ? searchResult.ipa : ipa
+
+    const wordMeaningsToShow = searchResult !== null ? searchResult.data : dictionaryWords
+    const notFoundWord =  searchError !== null && searchInputData !== "" ? searchInputData : clickedWord
+
+    console.log("wordToShow", wordToShow)
 
     return (
         <DictionaryContainer className="dinctionary-area-container">
+            <SearchBarContainer>
+                <SearchBarForm onSubmit={handleSearch}>
+                    <Input
+                        type="text"
+                        placeholder="Search..."
+                        value={searchInputData}
+                        onChange={handleInputChange}
+                    />
+                    <Img alt="search icon" src={search_icon}/>
+                </SearchBarForm>
+            </SearchBarContainer>
+
+            <HeadSectionContainer className="dinctionary-head">
+                <WordInfoWrapper>
+                    <Word>{wordToShow}</Word>
+                    <Ipa>{ipaToShow}</Ipa>
+                </WordInfoWrapper>
+            </HeadSectionContainer>
+
+            {/*{searchResult !== null ?*/}
+            {/*    <HeadSectionContainer className="dinctionary-head">*/}
+            {/*        <WordInfoWrapper>*/}
+            {/*            <Word>{searchInputData}</Word>*/}
+            {/*            <Ipa>{searchResult.ipa}</Ipa>*/}
+            {/*        </WordInfoWrapper>*/}
+            {/*    </HeadSectionContainer>*/}
+            {/*    :*/}
+            {/*    <>*/}
+            {/*        {clickedWord && (*/}
+            {/*            <HeadSectionContainer className="dinctionary-head">*/}
+            {/*                <WordInfoWrapper>*/}
+            {/*                    <Word>{clickedWord}</Word>*/}
+            {/*                    <Ipa>{ipa}</Ipa>*/}
+            {/*                </WordInfoWrapper>*/}
+            {/*            </HeadSectionContainer>*/}
+            {/*        )}*/}
+            {/*    </>*/}
+
+
+            {/*}*/}
 
             <DictionaryWrapper className="dinctionary-section">
-                {/*<SearchBarContainer>*/}
-                {/*    <SearchBar>*/}
-                {/*        <Img alt="search icon" src={search_icon}/>*/}
-                {/*        <Input*/}
-                {/*            type="text"*/}
-                {/*            placeholder="Search..."*/}
-                {/*        />*/}
-                {/*    </SearchBar>*/}
-                {/*</SearchBarContainer>*/}
-                {clickedWord && (
-                    <HeadSectionContainer className="dinctionary-head">
-                        <WordInfoWrapper>
-                            <Word>{clickedWord}</Word>
-                            <Ipa>{ipa}</Ipa>
-                        </WordInfoWrapper>
-                    </HeadSectionContainer>
-                )}
 
-                {dictionaryWords?.map((dw, index) =>
-                    <UserDictionaryWordItem
-                        key={index}
-                        wordItem={dw}
-                        setShowMeaningId={setShowMeaningId}
-                        showMeaningId={showMeaningId}
-                        setShowEditFormId={setShowEditFormId}
-                        showEditFormId={showEditFormId}
-                    />
-                )}
+                {searchResult !== null ?
+                    <>
+                        {searchResult.data?.map((result, index) =>
+                            <UserDictionarySearchResult
+                                key={index}
+                                result={result}
+                                setShowMeaningId={setShowMeaningId}
+                                showMeaningId={showMeaningId}
+                            />
+                        )}
+                    </>
+                    :
+                    <>
+                        {dictionaryWords?.map((dw, index) =>
+                            <UserDictionaryWordItem
+                                key={index}
+                                wordItem={dw}
+                                setShowMeaningId={setShowMeaningId}
+                                showMeaningId={showMeaningId}
+                            />
+                        )}
+                    </>
+                }
 
-
-                { wordNotFound !== null && clickedWord !== null  && (
+                { searchError !== null &&
+                    // searchInputData!== "") | (wordNotFound !== null && clickedWord!== null) &&
+                    (
                     <NotFoundContainer>
-                        <div>No results found for "{clickedWord}".</div>
+                        <div>No results found.</div>
+                    </NotFoundContainer>
+                )}
+
+                { wordNotFound !== null &&
+                    // searchInputData!== "") | (wordNotFound !== null && clickedWord!== null) &&
+                    (
+                    <NotFoundContainer>
+                        <div>No results found for "{notFoundWord}".</div>
                     </NotFoundContainer>
                 )}
 
@@ -63,16 +147,15 @@ function UserDictionaryArea({
 
 
 const SearchBarContainer = styled.div`
-  display: block;
   height: 50px;
+  padding: 0.5rem;
 `
-const SearchBar = styled.div`
+const SearchBarForm = styled.form`
   box-sizing: border-box;
   display: flex;
   //background-color: rgb(169,169,169, 0.5);
   border-radius: 8px;
   border: 2px solid rgb(169,169,169, 0.5);
-
 `
 const Img = styled.img`
   margin: 8px;
@@ -83,6 +166,7 @@ const Input = styled.input`
   border: none;
   background: none;
   color: #ddd;
+  padding-left: 0.5rem;
 
   &:hover {
     outline: none;
@@ -97,17 +181,20 @@ const WordInfoWrapper = styled.div`
 `
 
 const DictionaryWrapper = styled.div`
-    width: 100%;
-    padding: 16px;
+    //width: 100%;
+    padding: 0 1rem 1rem;
     overflow-y: auto;
     color: #ddd;
 `
 const DictionaryContainer = styled.div`
     display: flex;
+    flex-direction: column;
     max-width: 450px;
     width: 100%;
-  background-color: #333333;
+    background-color: #333333;
     border-radius: 8px;
+    box-sizing: border-box;
+
 `
 
 export default UserDictionaryArea
