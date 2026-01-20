b0182c5fb78de6e06792e4efe1e56f1fb2bc741d
jiyu13 <jiyucoding@yahoo.com>
2026-01-20 11:23:02 -0500

    - create DictionarySearchBar - apply DictionarySearchBar to UserDictionaryArea

---
+++ frontend/src/components/dictionary/DictionarySearchBar.jsx
@@ -0,0 +1,101 @@
+import search_icon from "../../assets/icons/search_icon.svg";
+import styled from "styled-components";
+import api from "../../api";
+
+export default function DictionarySearchBar({
+    setSearchInputData, searchInputData,  setSearchResult, setSearchError, searchError,
+}) {
+
+    function handleInputChange(e) {
+        const value = e.target.value
+        if (value === "") {
+            setSearchInputData("")
+            setSearchResult(null)
+        } else {
+            setSearchInputData(value)
+        }
+        setSearchError(null)
+    }
+
+    function handleSearch(e) {
+        e.preventDefault()
+
+        setSearchError(null)
+
+        api.get(`/word/word/${searchInputData}/`)
+            .then(res => {
+                const result = res.data
+                result.word = searchInputData
+                setSearchResult(result)
+            })
+            .catch (error => {
+                if (error.response) {
+                    setSearchError(error.response.data.error);
+                } else {
+                    console.log("network error", error.message);
+                }})
+    }
+
+
+    return (
+        <SearchBarContainer>
+            <SearchBarForm
+                onSubmit={handleSearch}
+                style={{border: searchError ? "2px solid #e74c3c" : "2px solid rgb(169,169,169, 0.5)"}}
+            >
+                <Input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchInputData}
+                    onChange={handleInputChange}
+                />
+                <Img
+                    alt="search icon"
+                    src={search_icon}
+                    onClick={handleSearch}
+                />
+            </SearchBarForm>
+
+            {searchError && (
+                 <SearchErrorMessage>"{searchInputData}" doesn't not exist</SearchErrorMessage>
+            )}
+
+        </SearchBarContainer>
+    )
+}
+
+const SearchBarContainer = styled.div`
+  //height: 50px;
+  padding: 0.5rem;
+`
+const SearchBarForm = styled.form`
+  box-sizing: border-box;
+  display: flex;
+  border-radius: 8px;
+`
+
+const SearchErrorMessage = styled.div`
+  color: #e74c3c;
+  font-size: 0.9rem;
+  padding-left: 0.5rem;  
+  padding-top: 0.5rem;  
+`
+
+const Img = styled.img`
+  margin: 8px;
+`
+const Input = styled.input`
+  width: 100%;
+  font-size: 1rem;
+  border: none;
+  background: none;
+  color: #ddd;
+  padding-left: 0.5rem;
+
+  &:hover {
+    outline: none;
+  }
+  &:focus {
+    outline: none;
+  }
+`
--- frontend/src/components/dictionary/UserDictionaryArea.jsx
+++ frontend/src/components/dictionary/UserDictionaryArea.jsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
-import search_icon from "../../assets/icons/search_icon.svg";
 import {useState} from "react";
 import UserDictionaryWordItem from "./UserDictionaryWordItem";
 import {HeadSectionContainer, Ipa, NotFoundContainer, Word} from "./StaffDictionaryArea";
-import api from "../../api";
 import UserDictionarySearchResult from "./UserDictionarySearchResult";
+import DictionarySearchBar from "./DictionarySearchBar";
 
 
 function UserDictionaryArea({
@@ -15,64 +14,20 @@ function UserDictionaryArea({
     const [showMeaningId, setShowMeaningId] = useState(null)
 
 
-    function handleInputChange(e) {
-        const value = e.target.value
-        if (value === "") {
-            setSearchInputData("")
-            setSearchResult(null)
-        } else {
-            setSearchInputData(value)
-        }
-        setSearchError(null)
-    }
-
-    function handleSearch(e) {
-        e.preventDefault()
-
-        setSearchError(null)
-
-        api.get(`/word/word/${searchInputData}/`)
-            .then(res => {
-                const result = res.data
-                result.word = searchInputData
-                setSearchResult(result)
-            })
-            .catch (error => {
-                if (error.response) {
-                    setSearchError(error.response.data.error);
-                } else {
-                    console.log("network error", error.message);
-                }})
-    }
 
     const wordToShow = searchResult !== null ? searchResult.word : clickedWord
     const ipaToShow = searchResult !== null ? searchResult.ipa : ipa
 
     return (
         <DictionaryContainer className="dinctionary-area-container">
-            <SearchBarContainer>
-                <SearchBarForm
-                    onSubmit={handleSearch}
-                    style={{border: searchError ? "2px solid #e74c3c" : "2px solid rgb(169,169,169, 0.5)"}}
-                >
-                    <Input
-                        type="text"
-                        placeholder="Search..."
-                        value={searchInputData}
-                        onChange={handleInputChange}
-                    />
-                    <Img
-                        alt="search icon"
-                        src={search_icon}
-                        onClick={handleSearch}
-                    />
-                </SearchBarForm>
 
-                {searchError && (
-                     <SearchErrorMessage>"{searchInputData}" doesn't not exist</SearchErrorMessage>
-                )}
-
-            </SearchBarContainer>
+            <DictionarySearchBar
+                setSearchInputData={setSearchInputData}
+                searchInputData={searchInputData}
+                setSearchResult={setSearchResult}
+                setSearchError={setSearchError}
+                searchError={searchError}
+            />
 
             <HeadSectionContainer className="dinctionary-head">
                 <WordInfoWrapper>
@@ -80,28 +35,6 @@ function UserDictionaryArea({
                     <Ipa>{ipaToShow}</Ipa>
                 </WordInfoWrapper>
             </HeadSectionContainer>
-
-            {/*{searchResult !== null ?*/}
-            {/*    <HeadSectionContainer className="dinctionary-head">*/}
-            {/*        <WordInfoWrapper>*/}
-            {/*            <Word>{searchInputData}</Word>*/}
-            {/*            <Ipa>{searchResult.ipa}</Ipa>*/}
-            {/*        </WordInfoWrapper>*/}
-            {/*    </HeadSectionContainer>*/}
-            {/*    :*/}
-            {/*    <>*/}
-            {/*        {clickedWord && (*/}
-            {/*            <HeadSectionContainer className="dinctionary-head">*/}
-            {/*                <WordInfoWrapper>*/}
-            {/*                    <Word>{clickedWord}</Word>*/}
-            {/*                    <Ipa>{ipa}</Ipa>*/}
-            {/*                </WordInfoWrapper>*/}
-            {/*            </HeadSectionContainer>*/}
-            {/*        )}*/}
-            {/*    </>*/}
-
-
-            {/*}*/}
 
             <DictionaryWrapper className="dinctionary-wrapper">
 
@@ -145,41 +78,7 @@ function UserDictionaryArea({
 }
 
 
-const SearchBarContainer = styled.div`
-  //height: 50px;
-  padding: 0.5rem;
-`
-const SearchBarForm = styled.form`
-  box-sizing: border-box;
-  display: flex;
-  border-radius: 8px;
-`
 
-const SearchErrorMessage = styled.div`
-  color: #e74c3c;
-  font-size: 0.9rem;
-  padding-left: 0.5rem;  
-  padding-top: 0.5rem;  
-`
-
-const Img = styled.img`
-  margin: 8px;
-`
-const Input = styled.input`
-  width: 100%;
-  font-size: 1rem;
-  border: none;
-  background: none;
-  color: #ddd;
-  padding-left: 0.5rem;
-
-  &:hover {
-    outline: none;
-  }
-  &:focus {
-    outline: none;
-  }
-`
 const WordInfoWrapper = styled.div`
   display: flex;
   align-items: baseline;
