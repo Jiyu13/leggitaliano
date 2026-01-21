a1077b664e22c5f05755a119775c09d838d052a3
jiyu13 <jiyucoding@yahoo.com>
2026-01-21 10:29:51 -0500

    check if word is+verb, if so render a different showcase of notes

--- frontend/src/components/dictionary/UserDictVerbConjugation.jsx
+++ frontend/src/components/dictionary/UserDictVerbConjugation.jsx
@@ -1,22 +1,38 @@
 function UserDictVerbConjugation({wordItem}) {
 
     const notes = wordItem["notes"]
+    const isVerb = wordItem.is_verb
 
     return (
         <>
-            {notes.map((note, index) => {
-                const splitNote = note.split(',')
-                const conjugationType = splitNote[0]
-                const conjugations = splitNote.slice(1).join(",")
-                return (
-                    <div key={index} style={{fontSize: "1rem"}}>
-                        <div style={{fontWeight: "bolder", textDecoration: "underline"}}>
-                            {conjugationType}:
-                        </div>
-                        <div key={index}>{conjugations}</div>
-                    </div>
-                )
-            })}
+            {isVerb ?
+                <>
+                    {notes.map((note, index) => {
+                        const splitNote = note.split(',')
+                        const conjugationType = splitNote[0]
+                        const conjugations = splitNote.slice(1).join(",")
+                        return (
+                            <div key={index} style={{fontSize: "1rem"}}>
+                                <div style={{fontWeight: "bolder", textDecoration: "underline"}}>
+                                    {conjugationType}:
+                                </div>
+                                <div key={index}>{conjugations}</div>
+                            </div>
+                        )
+                    })}
+                </>
+                :
+                <ul>
+                    {notes.map((note, index) => {
+                        return (
+                            <li key={index} style={{fontSize: "1rem"}}>
+                                <div key={index}>{note}</div>
+                            </li>
+                        )
+                    })}
+                </ul>
+
+            }
 
         </>
 
