004a373b37bc596ff397beca70b91af1ed2eb06a
jiyu13 <jiyucoding@yahoo.com>
2026-02-07 19:15:20 -0500

    to add "Add new meaning button" next to search bar

--- frontend/src/components/dictionary/DictionarySearchBar.jsx
+++ frontend/src/components/dictionary/DictionarySearchBar.jsx
@@ -1,14 +1,19 @@
 import search_icon from "../../assets/icons/search_icon.svg";
 import styled from "styled-components";
 import api from "../../api";
+import add_icon from "../../assets/icons/add_24dp.svg";
+import {useContext} from "react";
+import {UserContext} from "../../user-content/UserContent";
 
-const noop = () => {};
 
 
 export default function DictionarySearchBar({
-    setSearchInputData, searchInputData,  setSearchResult, setSearchError, searchError,
-    setShowMeaningId, setShowNewMeaningForm, setShowEditFormId
+    isStaffDictionary, setSearchInputData, searchInputData,  setSearchResult, setSearchError, searchError,
+    setShowMeaningId, setShowNewMeaningForm, isShowNewMeaningForm, setShowEditFormId
 }) {
+
+    const { currentUser} = useContext(UserContext)
+
 
     function handleInputChange(e) {
         const value = e.target.value
@@ -18,7 +23,7 @@ export default function DictionarySearchBar({
         } else {
             setSearchInputData(value)
         }
-        setShowNewMeaningForm(false)
+        setShowNewMeaningForm?.(false)
         setSearchError(null)
     }
 
@@ -48,22 +53,37 @@ export default function DictionarySearchBar({
 
     return (
         <SearchBarContainer>
-            <SearchBarForm
-                onSubmit={handleSearch}
-                style={{border: inputBorder}}
-            >
-                <Input
-                    type="text"
-                    placeholder="Search..."
-                    value={searchInputData}
-                    onChange={handleInputChange}
-                />
-                <Img
-                    alt="search icon"
-                    src={search_icon}
-                    onClick={handleSearch}
-                />
-            </SearchBarForm>
+            <SearchBarWrapper>
+                <SearchBarForm
+                    onSubmit={handleSearch}
+                    style={{border: inputBorder}}
+                >
+
+                    <Input
+                        type="text"
+                        placeholder="Search..."
+                        value={searchInputData}
+                        onChange={handleInputChange}
+                    />
+                    <Img
+                        alt="search icon"
+                        src={search_icon}
+                        onClick={handleSearch}
+                    />
+                </SearchBarForm>
+
+                {currentUser?.is_staff && isStaffDictionary && (
+                     <AddButton>
+                        <Img
+                            alt="add new meaning icon"
+                            src={add_icon}
+                            onClick={() => setShowNewMeaningForm(!isShowNewMeaningForm)}
+                        />
+                     </AddButton>
+                )}
+
+
+            </SearchBarWrapper>
 
             {searchError && (
                  <SearchErrorMessage>"{searchInputData}" doesn't not exist</SearchErrorMessage>
@@ -74,12 +94,30 @@ export default function DictionarySearchBar({
 }
 
 const SearchBarContainer = styled.div`
-  //height: 50px;
-  padding: 0.5rem;
+  padding: 0.5rem 0;
+  width: 100%;
+`
+
+const SearchBarWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  min-width: 0;     /* helps children shrink */
+  gap: 0.2rem;
 `
 const SearchBarForm = styled.form`
   box-sizing: border-box;
   display: flex;
+  border-radius: 8px;
+  width: 100%;
+  //flex: 1;
+  //min-width: 0;     /* KEY: allow shrink inside flex parent */
+  `
+
+const AddButton = styled.div`
+  //flex: 0 0 auto;
+  display: flex;
+  align-items: center;
+  border: 2px solid rgba(169,169,169, 1);
   border-radius: 8px;
 `
 
@@ -92,9 +130,12 @@ const SearchErrorMessage = styled.div`
 
 const Img = styled.img`
   margin: 8px;
+  flex: 0 0 auto; /* don't steal width from input */
 `
 const Input = styled.input`
-  width: 100%;
+  flex: 1;          /* KEY: flex instead of width:100% */
+  min-width: 0;     /* KEY: prevent overflow */
+  //width: 100%;
   font-size: 1rem;
   border: none;
   background: none;
