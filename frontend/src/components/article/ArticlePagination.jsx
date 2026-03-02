84094b707afb6c46630b9b26268520e202e83dc5
jiyu13 <jiyucoding@yahoo.com>
2026-03-02 11:50:10 -0500

    feat: make arrow btns inline svg - import svg icon as components - style + force paths to follow currentColor

--- frontend/src/components/article/ArticlePagination.jsx
+++ frontend/src/components/article/ArticlePagination.jsx
@@ -1,5 +1,5 @@
-import arrow_left from "../../assets/icons/arrow_left_24dp.svg"
-import arrow_right from "../../assets/icons/arrow_right_24dp.svg"
+import {ReactComponent as ArrowLeftIcon} from "../../assets/icons/arrow_left_24dp.svg"
+import {ReactComponent as ArrowRightIcon} from "../../assets/icons/arrow_right_24dp.svg"
 import {ReactComponent as CheckIcon} from "../../assets/icons/check_24dp.svg"
 import styled from "styled-components";
 import {UserContext} from "../../user-content/UserContent";
@@ -19,26 +19,29 @@ function ArticlePagination({
     return(
         <PaginationContainer className="pagination-container">
 
-            <ArrowContainer
-                className="arrow-container"
+            <ArrowIcon
+                alt="previous page icon"
                 style={{visibility: leftArrow}}
                 onClick={handlePrevPage}
-            >
-                <ArrowImg src={arrow_left} alt="previous page icon"/>
-            </ArrowContainer>
+                as={ArrowLeftIcon}
+            />
 
             <NumberContainer className="number-container">
                 <NumberDisplay>{currentPage} / {pages}</NumberDisplay>
             </NumberContainer>
 
             {currentPage === pages ?
-                <ArrowContainer onClick={handleFinishReading}>
-                    <CheckIconStyled $isFinished={isFinished} aria-label="finish reading icon" />
-                </ArrowContainer>
+                <CheckIconStyled
+                    onClick={handleFinishReading}
+                    $isFinished={isFinished}
+                    aria-label="finish reading icon"
+                />
                 :
-                <ArrowContainer onClick={handleNextPage}>
-                    <ArrowImg src={arrow_right} alt="next page icon"/>
-                </ArrowContainer>
+                <ArrowIcon
+                    onClick={handleNextPage}
+                    as={ArrowRightIcon}
+                    alt="next page icon"
+                />
             }
 
         </PaginationContainer>
@@ -49,36 +52,45 @@ const PaginationContainer = styled.div`
   grid-template-columns: repeat(3, 1fr);
   width: 100%;
 `
-const ArrowContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0.5rem 0;
-  
-  &:hover  {
-    background-color: #4a4a4a;
-    border-radius: 8px;
-  }
-`
 
 const CheckIconStyled = styled(CheckIcon)`
   width: 100%;
   height: 36px;
-  color: ${({ $isFinished }) => ( $isFinished ? "rgb(23,188,90)" : "#fff" )};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 0;
+  border-radius: 8px;
+  
+  color: ${({ $isFinished }) => ( $isFinished ? "#fff" : "" )};
   transition: color 120ms ease;
-  &:hover {
-    color: ${({ $isFinished }) => ($isFinished ? "rgb(23,188,90)" : "#fff")};
+  background-color: ${({ $isFinished }) => ( $isFinished ? "rgba(23,188,90, 1)" : "" )};
+  &:hover  {
+    color: #fff;
+    cursor: pointer;
+    background-color: ${({ $isFinished }) => ( $isFinished ? "rgba(23,188,90, 1)" : "#4a4a4a" )};
   }
 `
 
-const ArrowImg = styled.img`
+const ArrowIcon = styled.svg`
   width: 100%;
   height: 36px;
-  //filter: brightness(0) saturate(100%) invert(40%) sepia(0%) saturate(0%) hue-rotate(0deg);
-
-  &:hover {
-    filter: brightness(0) invert(1)
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 0;
+  border-radius: 8px;
+  
+  & path {
+    fill: currentColor;
   }
+  
+  &:hover  {
+    color: #fff;
+    cursor: pointer;
+    background-color: #4a4a4a ;
+  }
+  
 `
 const NumberContainer = styled.div`
   display: flex;
